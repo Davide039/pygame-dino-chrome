52c3a8c59cf749b1d4e76394dbced94a4837d2b5
Davide039 <65111617+Davide039@users.noreply.github.com>
2021-09-05 14:25:16 +0200

    Update dino.py

--- dino.py
+++ dino.py
@@ -1,5 +1,6 @@
+import pygame
 import random
-import sys, pygame
+import sys
 
 pygame.init()
 
@@ -24,113 +25,114 @@ isJumping = False
 v = 8
 m = 1
 
-class Player:
-    def __init__(self, x, y) -> None:
-        self.x = x
-        self.y = y
-        self.images = [
-            pygame.image.load("dino_first.jpg"),
-            pygame.image.load("dino_second.jpg")
-        ]
-        self.image_index = 0
-        self.current = pygame.transform.scale(self.images[self.image_index % len(self.images)],
-                                              (78, 84)).convert_alpha()
-        self.current.set_colorkey((255, 255, 255))
-        self.current_rect = self.current.get_rect()
+class PlayerSprite(pygame.sprite.Sprite):
+	def __init__(self, x, y) -> None:
+		super().__init__()
+		self.x = x
+		self.y = y
 
-    def do_animation(self):
-        self.image_index += 1
-        self.current = pygame.transform.scale(self.images[self.image_index % len(self.images)],
-                                              (78, 84)).convert_alpha()
-        self.current.set_colorkey((255, 255, 255))
-        self.current_rect = self.current.get_rect()
+		self.acount = 0
+		self.images = [pygame.image.load(f'./img/dino_{f}.jpg') for f in ["first", "second"]]
+		self.image = self.images[self.acount % len(self.images)]
+		self.image.set_colorkey((255, 255, 255))
+		self.rect = self.image.get_rect()
 
+	def update(self):
+		self.acount += 1
+		self.image = self.images[self.acount % len(self.images)]
+		self.image.set_colorkey((255, 255, 255))
 
-player = Player(30, 345)
+player = PlayerSprite(30, 345)
 
+class Obstacle(pygame.sprite.Sprite):
+	def __init__(self, x, y) -> None:
+		super().__init__()
+
+		self.x = x
+		self.y = y
+
+		self.image = pygame.image.load("img/obstacle.jpg")
+		self.rect = self.image.get_rect(midtop=(self.x, self.y))
 
 class Obstacles:
-    def __init__(self) -> None:
-        self.obstacles = []
-        self.obstacle = pygame.image.load("obstacle.jpg").convert_alpha()
-        self.obstacle.set_colorkey((255, 255, 255))
+	def __init__(self) -> None:
+		self.obstacles = []
 
-    def create_obstacle(self):
-        self.obstacles.append(self.obstacle.get_rect(midtop=(1350, 345)))
+	def create_obstacle(self):
+		self.obstacles.append(Obstacle(1350, 345))
 
-    def move_obstacles(self):
-        for obst in self.obstacles:
-            obst.centerx -= speed
-        return self.obstacles
+	def move_obstacles(self):
+		for obst in self.obstacles:
+			obst.rect.centerx -= speed
+		return self.obstacles
 
-    def draw_obstacles(self):
-        for obst in self.obstacles:
-            screen.blit(self.obstacle, obst)
+	def draw_obstacles(self):
+		for obst in self.obstacles:
+			screen.blit(obst.image, obst.rect)
 
-    def check_collision(self):
-        for obstacle in self.obstacles:
-            if player.current_rect.colliderect(obstacle):
-                return False
-        return True
-
+	def check_collision(self):
+		for obst in self.obstacles:
+			if obst.rect.colliderect(player.rect):
+				return False
+		return True
 
 obstacles = Obstacles()
 
 while True:
-    screen.fill((255, 255, 255))
+	screen.fill((255, 255, 255))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == CHANGE_PLAYER_FRAMES and game_active:
-            player.do_animation()
-        if event.type == GENERATE_OBSTACLE:
-            obstacles.create_obstacle()
-            pygame.time.set_timer(GENERATE_OBSTACLE, random.choice(times))
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+		if event.type == CHANGE_PLAYER_FRAMES and game_active:
+			player.update()
+		if event.type == GENERATE_OBSTACLE:
+			obstacles.create_obstacle()
+			pygame.time.set_timer(GENERATE_OBSTACLE, random.choice(times))
 
-    keys = pygame.key.get_pressed()
+	keys = pygame.key.get_pressed()
 
-    if game_active:
-        if not isJumping:
-            if keys[pygame.K_SPACE]:
-                isJumping = True
+	if game_active:
+		if not isJumping:
+			if keys[pygame.K_SPACE]:
+				isJumping = True
 
-        if isJumping:
-            F = (1 / 2) * m * (v ** 2)
-            player.y -= F
+		if isJumping:
+			F = (1 / 2) * m * (v ** 2)
+			player.y -= F
 
-            v -= 0.4
+			v -= 0.4
 
-            if v < 0:
-                m = -0.5
-            if player.y >= 345:
-                isJumping, v, m = False, 8, 1
-    else:
-        if keys[pygame.K_SPACE]:
-            game_active = True
-            speed = 5
+			if v < 0:
+				m = -0.5
+			if player.y >= 345:
+				isJumping, v, m = False, 8, 1
+	else:
+		if keys[pygame.K_SPACE]:
+			game_active = True
+			speed = 5
 
-    if game_active:
-        game_active = obstacles.check_collision()
-    else:
-        obstacles.obstacles.clear()
-        speed = 0
-        screen.blit(pygame.image.load("restart.jpg"),
-					pygame.image.load("estart.jpg").get_rect(center=(width / 2, height / 2)))
+	if game_active:
+		game_active = obstacles.check_collision()
+	else:
+		obstacles.obstacles.clear()
+		speed = 0
+		screen.blit(pygame.image.load("img/restart.jpg"),
+					pygame.image.load("img/restart.jpg").get_rect(center=(width / 2, height / 2)))
 
-    groundx -= speed
-    if groundx <= -2400:
-        groundx = 0
+	groundx -= speed
+	if groundx <= -2400:
+		groundx = 0
 
-    screen.blit(ground, (groundx, 410))
-    screen.blit(ground, (groundx + 2400, 410))
+	screen.blit(ground, (groundx, 410))
+	screen.blit(ground, (groundx + 2400, 410))
 
-    screen.blit(player.current, (player.x, player.y))
+	screen.blit(player.image, (player.x, player.y))
 
-    obstacles.obstacles = obstacles.move_obstacles()
-    obstacles.draw_obstacles()
+	obstacles.obstacles = obstacles.move_obstacles()
+	obstacles.draw_obstacles()
 
-    pygame.display.flip()
-    pygame.display.update()
-    clock.tick(120)
+	pygame.display.flip()
+	pygame.display.update()
+	clock.tick(120)
